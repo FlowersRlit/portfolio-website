e5bcb0a7b95c52d5f722b03bc0e35629f3ed6551
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-13 06:35:53 +0700

    removed generated text lines from react app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ function App() {
   return (
     <div className="App container">
       <Navbar />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
