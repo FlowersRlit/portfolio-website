457e6da1067a26a62760d5b9a806fb2a5e995263
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-18 19:17:05 +0700

    created new folder for navigation section components

---
+++ src/navigation/NavButtons.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavButtons = () => {
+  return (
+    <div className="mx-6">
+      <a id="Beranda">Beranda</a>
+      <a id="Portfolio">Portfolio</a>
+      <a id="Kontak">Kontak</a>
+    </div>
+  );
+};
+
+export default NavButtons;
