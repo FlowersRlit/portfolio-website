559fda11c0bd80be7d6c8ded13a5ff37b6d198fc
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-22 21:51:18 +0700

    Created simple footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex w-full justify-center bg-primary">
+      <p className="my-4 text-white">
+        Copyright &copy; <a href="https://linktr.ee/FlowersRlit">FlowersRlit</a>
+        2023 - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
