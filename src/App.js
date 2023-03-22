6e09f8ed6c150733ef9b7b5ab839ab8befa8ac1a
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-22 21:57:35 +0700

    Added router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import Beranda from "./beranda/Beranda";
+import Footer from "./footer/Footer";
+import Navbar from "./navigation/Navbar";
+import Portfolio from "./portfolio/Portfolio";
 
 function App() {
   return (
-    <div className="App container">
+    <div className="font-montserrat">
       <Navbar />
-      <header className="App-header"></header>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Beranda />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
