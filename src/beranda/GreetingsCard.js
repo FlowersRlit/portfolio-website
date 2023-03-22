49a4f42f612067acbe67ca42f45a55eb1433cf5e
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-22 21:50:39 +0700

    Main profile

---
+++ src/beranda/GreetingsCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import avatar from "../assets/kelas.jpg";
+
+const GreetingsCard = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 md:flex-row">
+      <div className="drop h-40 w-40 overflow-hidden rounded-2xl bg-black shadow-primary">
+        <img
+          src={avatar}
+          className="h-full w-full object-cover object-bottom"
+        />
+      </div>
+      <div className="text-center md:text-start">
+        <h1 className="text-lg font-semibold text-primary md:text-5xl">
+          Greetings.
+        </h1>
+        <p className="text-xs md:text-base">
+          Halo, namaku Chika Kirana Syafruddin
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GreetingsCard;
