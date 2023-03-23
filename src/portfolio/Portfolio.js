fa414b4f848a1abd5edb3775507c4702984a8abf
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-24 01:03:06 +0700

    added Achievement component

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Cards from "./Cards";
+import Quote from "./Quote";
+import Achievement from "./Achievement";
+import Footer from "../footer/Footer";
 
 const Portfolio = () => {
   return (
-    <div>
-      <Cards />
-    </div>
+    <>
+      <div className="container h-auto">
+        <Quote />
+        <Cards />
+        <Achievement />
+      </div>
+    </>
   );
 };
 
