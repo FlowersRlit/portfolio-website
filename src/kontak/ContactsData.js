50ed12e76c41c67a07d8e6002795fb93033d4aec
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-24 00:11:12 +0700

    added hover action

--- src/kontak/ContactsData.js
+++ src/kontak/ContactsData.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContactsData = ({ Name, Svg, Link }) => {
   return (
     <a
-      className="relative mx-10 my-10 flex w-[15rem] items-center justify-center rounded-full bg-primary py-4 text-center text-white sm:w-[20rem] md:w-[30rem] lg:w-[50rem]"
+      className="relative mx-10 my-10 flex w-[15rem] items-center justify-center rounded-full bg-primary py-4 text-center text-white hover:bg-primary-focus sm:w-[20rem] md:w-[30rem] lg:w-[50rem]"
       href={Link}
       target="_blank"
       rel="noreferrer">
