55e137a592fbcc578cd85cac97e8aba31c1448db
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-24 01:08:24 +0700

    created Achievement component

---
+++ src/portfolio/Achievement.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Achievement = () => {
+  return (
+    <>
+      <h1 className="pt-40 pb-10 text-center text-5xl font-bold text-primary">
+        Hall of Acknowledgement
+      </h1>
+      <div className="flex items-start justify-between">
+        {/* left section */}
+        <div className="h-20 w-20 border-2 border-red-500"></div>
+        {/* right section */}
+        <div>Hi</div>
+      </div>
+    </>
+  );
+};
+
+export default Achievement;
