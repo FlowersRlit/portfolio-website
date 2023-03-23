422fc4bd5e713dd8b32b248061f32e32aff286de
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-23 13:27:23 +0700

    generated new themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,56 @@ module.exports = {
         padding: "1rem",
       },
       fontFamily: {
-        montserrat: "'Montserrat', sans-serif"
+        montserrat: "'Montserrat', sans-serif",
       },
     },
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["garden", "night"],
+    themes: [
+      {
+        //light theme
+        calmblu: {
+          primary: "#6384E9",
+
+          secondary: "#cce7ff",
+
+          accent: "#3dcea7",
+
+          neutral: "#2D223F",
+
+          "base-100": "#F3F2F7",
+
+          info: "#2CB6D8",
+
+          success: "#22D377",
+
+          warning: "#C48208",
+
+          error: "#EF4E6B",
+        },
+        // dark theme
+        nightblu: {
+          primary: "#1689af",
+
+          secondary: "#71bcc6",
+
+          accent: "#00b557",
+
+          neutral: "#29253C",
+
+          "base-100": "#1D2734",
+
+          info: "#3668D3",
+
+          success: "#45DE8A",
+
+          warning: "#FBB904",
+
+          error: "#E55D7A",
+        },
+      },
+    ],
     styled: true,
     base: true,
     utils: true,
