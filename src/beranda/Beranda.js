d1474e4cddc42bb0ead21530e47adafc0f7068a2
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-24 01:05:49 +0700

    added Introduction component

--- src/beranda/Beranda.js
+++ src/beranda/Beranda.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Gallery from "./Gallery";
 import GreetingsCard from "./GreetingsCard";
+import Introduction from "./Introduction";
 
 const Beranda = () => {
   return (
     // background start
-    <div className="container my-40 flex h-screen flex-col items-center justify-center gap-6 md:my-28">
+    <div className="container my-40 flex h-auto flex-col items-center justify-center gap-6 md:my-28">
       <GreetingsCard />
-
+      <Introduction />
       <Gallery />
     </div>
   );
